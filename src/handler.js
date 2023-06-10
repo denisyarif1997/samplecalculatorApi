0e2158d077edef2b01f684f30f34f35d540c92ce
Denis <48453257+denisyarif@users.noreply.github.com>
2023-06-10 08:44:27 +0700

    tambah fungsi edit

--- src/handler.js
+++ src/handler.js
@@ -66,5 +66,33 @@ const getNoteByIdHandler = (request, h) => {
       return response;
     };
 
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const { title, tags, body } = request.payload;
+    const updatedAt = new Date().toISOString();
+    const index = notes.findIndex((note) => note.id === id);
 
-module.exports = { addNoteHandler,getAllNotesHandler,getNoteByIdHandler };
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+        }
+
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan Berhasil Diperbaharui',
+        });
+        response.code (200);
+        return response;
+    }
+
+    const response = h.response({
+        status:'fail'
+    })
+}
+
+
+module.exports = { addNoteHandler,getAllNotesHandler,getNoteByIdHandler,editNoteByIdHandler };
