15a5ee91bd67baf1358566dbf9c4900589f13e8d
Denis <48453257+denisyarif@users.noreply.github.com>
2023-06-12 15:03:02 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Hapi = require('@hapi/hapi');
+ 
+const host = 'localhost';
+const port = 3000;
+
+const server = Hapi.Server({
+  host: host,
+  port: port
+});
+
+//routes
+require('./routes')(server);
+
+//start server
+const init = async () => {
+  await server.start();
+  console.log("Server up! Port: " + port);
+}
+
+init();
--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,89 @@
-const { addNoteHandler,getAllNotesHandler,getNoteByIdHandler,editNoteByIdHandler,deleteNoteByIdHandler } = require("./handler");
+module.exports = function (server) {
+    //route about
+    server.route({
+        method: 'GET',
+        path: '/calculator/about',
+        handler: function (request, h) {
 
-const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-    },
-    {
+          var data = {
+            message: 'API calculator'
+          };
+
+          return data;
+
+        }
+      });
+
+      //route - sum
+    server.route({
         method: 'GET',
-        path:'/notes',
-        handler: getAllNotesHandler,
-    },
-    {
+       
+       path: '/calculator/sum/{num1}+{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                answer: num1 + num2
+            };
+    
+            return data;
+        }
+    });
+
+    //route - subtraction
+    server.route({
         method: 'GET',
-        path: '/notes/{id}',
-        handler: getNoteByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: editNoteByIdHandler,
-    },
-    {
-        method: 'Delete',
-        path: '/notes/{id}',
-        handler: deleteNoteByIdHandler,
-    }
-  ];
-   
-  module.exports = routes;
+       
+       path: '/calculadora/sub/{num1}-{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                resposta: num1 - num2
+            };
+    
+            return data;
+        }
+    });
+
+    //route - multiplication
+    server.route({
+        method: 'GET',
+       
+       path: '/calculadora/multi/{num1}*{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                resposta: num1 * num2
+            };
+    
+            return data;
+        }
+    });
+
+    //route - division
+    server.route({
+        method: 'GET',
+       
+       path: '/calculadora/div/{num1}/{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                resposta: num1 / num2
+            };
+    
+            return data;
+        }
+    });
+
+}
